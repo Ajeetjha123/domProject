e4c8167550a616dc3e30775eba9a174a351167f7
Ajeetjha123 <84560142+Ajeetjha123@users.noreply.github.com>
2023-04-26 15:12:36 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,6 @@
+var headerTitle  = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+headerTitle.textContent='Hello';
+header.style.borderBottom = 'solid 3px #000';
+var addItem = document.getElementsByClassName('title')[0];
+addItem.style.color = 'green';
